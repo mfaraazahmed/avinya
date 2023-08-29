e1bc671fa965c446be8a4df513637580da941c73
Mohammed Faraaz Ahmed <91116035+mfaraazahmed@users.noreply.github.com>
2023-08-29 20:33:30 +0530

    fade in animation

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-function animateOnScroll(elements, animationClass) {
-  elements.forEach(element => {
-      const elementTop = element.getBoundingClientRect().top;
-      const windowHeight = window.innerHeight;
-      const triggerOffset = windowHeight / 2;
+// function animateOnScroll(elements, animationClass) {
+//   elements.forEach(element => {
+//       const elementTop = element.getBoundingClientRect().top;
+//       const windowHeight = window.innerHeight;
+//       const triggerOffset = windowHeight / 2;
 
-      if (elementTop < triggerOffset) {
-          element.classList.add(animationClass);
-      }
-  });
-}
+//       if (elementTop < triggerOffset) {
+//           element.classList.add(animationClass);
+//       }
+//   });
+// }
 
-const elementsToAnimate = document.querySelectorAll('.sub-title, .animate-image, .animate-text, .img1, .img2, .img3, .img4');
+// const elementsToAnimate = document.querySelectorAll('.sub-title, .animate-image, .animate-text, .img1, .img2, .img3, .img4');
 
-window.addEventListener('scroll', () => {
-  animateOnScroll(elementsToAnimate, 'animate');
-});
+// window.addEventListener('scroll', () => {
+//   animateOnScroll(elementsToAnimate, 'animate');
+// });
+
