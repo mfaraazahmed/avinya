ee632cd649271b232a8a676a6aab62a9d5ecf663
Mohammed Faraaz Ahmed <91116035+mfaraazahmed@users.noreply.github.com>
2023-08-29 19:38:57 +0530

    need change of animation

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 function animateOnScroll(elements, animationClass) {
   elements.forEach(element => {
-    const elementTop = element.getBoundingClientRect().top;
-    const windowHeight = window.innerHeight;
-    const triggerOffset = windowHeight / 0.9 - element.clientHeight / 2;
+      const elementTop = element.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
+      const triggerOffset = windowHeight / 2;
 
-    if (elementTop < triggerOffset) {
-      element.classList.add(animationClass);
-    }
+      if (elementTop < triggerOffset) {
+          element.classList.add(animationClass);
+      }
   });
 }
 
-const subTitles = document.querySelectorAll('.sub-title');
-const animateImageElements = document.querySelectorAll('.animate-image, .animate-text');
-const animateImageElementsMore = document.querySelectorAll('.img1, .img2, .img3, .img4');
+const elementsToAnimate = document.querySelectorAll('.sub-title, .animate-image, .animate-text, .img1, .img2, .img3, .img4');
 
 window.addEventListener('scroll', () => {
-  animateOnScroll(subTitles, 'animate');
-  animateOnScroll(animateImageElements, 'animate');
-  animateOnScroll(animateImageElementsMore, 'animate');
+  animateOnScroll(elementsToAnimate, 'animate');
 });
